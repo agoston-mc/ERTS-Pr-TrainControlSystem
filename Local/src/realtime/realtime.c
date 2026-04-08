d3cc239656031d15cd8c6cec674c7c2f0cefa101
highpn <marcibarat@gmail.com>
2026-04-08 16:48:00 +0200

    refactor: reorganize includes and enhance shared memory handling in realtime.c

--- Local/src/realtime/realtime.c
+++ Local/src/realtime/realtime.c
@@ -2,7 +2,13 @@
 #include <sys/msg.h>
 #include <stdint.h>
 #include <stdbool.h>
-
+#include <fcntl.h>           /* For O_* constants */
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdatomic.h>
 struct DoorControl {
     // Timing
     uint16_t target_ms;      // Desired close time
@@ -14,8 +20,23 @@ struct DoorControl {
     uint16_t resume_wait_ms; // Pause before retry
 };
 
+int id;
 
 int main(int argc, char**argv){
+    
+    char buffer[4];
+    char dest[4];
+    int shm_fd = shm_open("/id", O_CREAT | O_RDWR, 0777);
+    
+    if(ftruncate(shm_fd,4)==-1){
+            printf("error ftruncate %s in line %d",strerror(errno),__LINE__);
+            exit(-1);
+    }
+    void*d=mmap(dest,4,PROT_WRITE | PROT_READ,MAP_SHARED,shm_fd,0);
+    id=atomic_fetch_add((int*)d,1);
+    printf("%d \n",id);
+    
+    sleep(2);
 	int msqid = 12345;
 	struct message {
         long type;
@@ -27,5 +48,14 @@ int main(int argc, char**argv){
     msgrcv(msqid,(void *)&msg, sizeof(msg), msgtyp, 0);
 
 	printf("Message : %s", msg.text);
+    munmap(dest,sizeof(id));
+    close(shm_fd);
+    if(shm_unlink("id")==-1){
+        if(errno == ENOENT )
+            return 0;
+        printf("error shm_unlink %s in line %d",strerror(errno),__LINE__);
+        exit(-1);
+    }
+
 	return 0;
 }
